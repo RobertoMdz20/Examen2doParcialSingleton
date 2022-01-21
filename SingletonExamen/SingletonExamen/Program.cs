ec85696e25b7bdc7dad6c664b630ab3e7fbe6a7b
ROBERTO ALEJANDRO MENDOZA VELEZ <e1312856816@live.uleam.edu.ec>
2022-01-20 21:06:50 -0500

    Solución inicial

---
+++ SingletonExamen/SingletonExamen/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SingletonExamen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Generamos la instancia a partir del metodo creado para realizar este proceso.
+            Singleton Instacia1 = Singleton.GenerarInstancia();
+
+            //Ingresamos el mensaje de bienvenida.
+            Instacia1.IngresarMsj("Sean coordialmente invitados a esta app, espero que la disfruten y les sea de ayuda.");
+            //Mostramos el mensaje de bienvenida.
+            Console.WriteLine(Instacia1);
+            Console.WriteLine("\n****----------****");
+
+            //Para corroborar el funcionamiento del patrón, creamos otra instancia.
+            Singleton Instacia2 = Singleton.GenerarInstancia();
+            //Mostramos la instancia 2
+            Console.WriteLine(Instacia2);
+
+            /*El correcto funcionamiento del patrón de diseño singleton que solo se puede crear una instancia, se demuestra al momento de
+            mostrar la instancia 2, mostrando el mismo mensaje de la instancia 1, ya que siempre devuelve la misma instancia.*/
+        }
+    }
+}
---
+++ SingletonExamen/SingletonExamen/Singleton.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonExamen
+{
+    class Singleton
+    {
+        //Declaramos la instancia que será utilizada.
+        private static Singleton InstanciaUnica;
+
+        //Declaramos una propiedad para establecer un texto.
+        private string mensaje;
+        //Constructor privado del singleton.
+        private Singleton()
+        {
+            mensaje = "";
+        }
+        //Metodo para poder generar la instancia unica
+        public static Singleton GenerarInstancia()
+        {
+            //Se verifica la existencia de la instancia.
+            if (InstanciaUnica == null)
+            {
+                //Si la instancia no existe, se procede a isntanciar.
+                InstanciaUnica = new Singleton();
+                //Muestra un mensaje de notificación de creación de la instancia.
+                Console.WriteLine("\nLa instancia fue creada");
+            }
+            return InstanciaUnica;
+        }
+
+        //Metodo para mostrar un mensaje de bienvenida.
+        public override string ToString()
+        {
+            return String.Format("\n**** Mensaje de bienvenida: {0} ****", mensaje);
+        }
+        //Metodo para ingresar el mensaje.
+        public void IngresarMsj(string Imsj)
+        {
+            mensaje = Imsj;
+        }
+
+    }
+}
